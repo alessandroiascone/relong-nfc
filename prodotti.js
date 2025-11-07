8659aac05852aa4a1bf7c4c5dbdc4b0b188f9cac
Re Long Informatica – Telefonia, Energia & Assistenza PC <alessandro.iascone@gmail.com>
2025-11-07 18:48:58 +0100

    Update prodotti.js

--- prodotti.js
+++ prodotti.js
@@ -1,79 +1,45 @@
-window.prodotti = [
+[
   {
-    "id": "iphone17pro1tb",
+    "brand": "Samsung",
+    "nome": "Galaxy A17 256GB",
+    "prezzo": 315.00,
+    "img": "./imgcatalogo/samsung-galaxy-a17-256.jpg",
+    "storage": "256GB",
+    "colore": "Black",
+    "promo": true
+  },
+  {
     "brand": "Apple",
     "nome": "iPhone 17 Pro 1TB",
-    "prezzo": 1993.8,
-    "img": "imgcatalogo/iphone17pro1tb.webp",
+    "prezzo": 1993.00,
+    "img": "./imgcatalogo/iphone-17-pro-1tb.jpg",
+    "storage": "1TB",
+    "colore": "Natural Titanium",
     "top": true
   },
   {
-    "id": "iphone17air256",
-    "brand": "Apple",
-    "nome": "iPhone 17 Air 256GB",
-    "prezzo": 1199.0,
-    "img": "imgcatalogo/iphone17air256.webp",
-    "novita": true
-  },
-  {
-    "id": "iphone17promax256",
-    "brand": "Apple",
-    "nome": "iPhone 17 Pro Max 256GB",
-    "prezzo": 1657.8,
-    "img": "imgcatalogo/iphone17promax256.webp",
-    "promo": true
-  },
-  {
-    "id": "samsungA17_256",
-    "brand": "Samsung",
-    "nome": "Samsung Galaxy A17 256GB",
-    "prezzo": 315.0,
-    "img": "imgcatalogo/samsungA17_256.webp",
-    "promo": true
-  },
-  {
-    "id": "samsungS25FE_256",
-    "brand": "Samsung",
-    "nome": "Samsung Galaxy S25 FE 256GB",
-    "prezzo": 773.9,
-    "img": "imgcatalogo/samsungS25FE_256.webp",
-    "novita": true
-  },
-  {
-    "id": "samsungA56_5g",
-    "brand": "Samsung",
-    "nome": "Samsung Galaxy A56 5G",
-    "prezzo": 483.8,
-    "img": "imgcatalogo/samsungA56_5g.webp"
-  },
-  {
-    "id": "oppo14F_256",
-    "brand": "OPPO",
-    "nome": "OPPO 14F 256GB",
-    "prezzo": 255.9,
-    "img": "imgcatalogo/oppo14F_256.webp"
-  },
-  {
-    "id": "oppoA5pro",
-    "brand": "OPPO",
-    "nome": "OPPO A5 Pro",
-    "prezzo": 255.9,
-    "img": "imgcatalogo/oppoA5pro.webp"
-  },
-  {
-    "id": "honor400lite_256",
     "brand": "Honor",
     "nome": "Honor 400 Lite 256GB",
-    "prezzo": 159.9,
-    "img": "imgcatalogo/honor400lite_256.webp",
-    "promo": true
+    "prezzo": 159.90,
+    "img": "./imgcatalogo/honor-400-lite-256.jpg",
+    "storage": "256GB",
+    "colore": "Blue",
+    "novita": true
   },
   {
-    "id": "zfold7_256",
+    "brand": "OPPO",
+    "nome": "OPPO 14F 256GB",
+    "prezzo": 255.90,
+    "img": "./imgcatalogo/oppo-14f-256.jpg",
+    "storage": "256GB",
+    "colore": "Green"
+  },
+  {
     "brand": "Samsung",
-    "nome": "Samsung Galaxy Z Fold7 256GB",
-    "prezzo": 1977.0,
-    "img": "imgcatalogo/zfold7_256.webp",
-    "top": true
+    "nome": "Galaxy S25 FE 256GB",
+    "prezzo": 773.90,
+    "img": "./imgcatalogo/samsung-galaxy-s25-fe-256.jpg",
+    "storage": "256GB",
+    "colore": "Graphite"
   }
-];
+]
